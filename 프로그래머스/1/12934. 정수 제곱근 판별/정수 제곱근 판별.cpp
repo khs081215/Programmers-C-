d9ba3642f23d83ef50d56c084634e047863d4009
khs081215 <39721769+khs081215@users.noreply.github.com>
2025-12-26 09:08:35 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.02 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+long long solution(long long n) {
+    long long answer = 0;
+    if(sqrt(n)-(int)sqrt(n)==0){
+        return (sqrt(n)+1)*(sqrt(n)+1);
+    }
+    else return -1;
+}
