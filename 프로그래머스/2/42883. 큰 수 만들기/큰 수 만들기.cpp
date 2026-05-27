7a6285df7fbf23f61a9a0f620a0e4cb764898f46
khs081215 <39721769+khs081215@users.noreply.github.com>
2026-05-27 09:30:10 +0900

    [level 2] Title: 큰 수 만들기, Time: 3.46 ms, Memory: 5.56 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+string solution(string number, int k) {
+string answer = "";
+    
+    for (int i = 0; i < number.length(); i++) {
+        while (k > 0 && !answer.empty() && answer.back() < number[i]) {
+            answer.pop_back(); 
+            k--;
+        }
+        answer.push_back(number[i]);
+    }
+    
+    if (k > 0) {
+        answer = answer.substr(0, answer.length() - k);
+    }
+    
+    return answer;
+    //
+}
